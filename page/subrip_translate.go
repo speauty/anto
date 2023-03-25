2a2134287e62d6dc3ed91ffbff53aa513d75af5b
speauty <speauty@163.com>
2023-03-25 22:11:15 +0800

    update（translator）：调整comboBox模式数据；

--- page/subrip_translate.go
+++ page/subrip_translate.go
@@ -4,13 +4,23 @@ import (
 	"github.com/lxn/walk"
 	. "github.com/lxn/walk/declarative"
 	"sync"
+	"translator/tst/tt_translator/ling_va"
 	"translator/tst/tt_ui/pack"
 	"translator/util"
 )
 
+type LangItem struct {
+	Key  string
+	Name string
+}
+
 var (
 	apiSubripTranslate  *SubripTranslate
 	onceSubripTranslate sync.Once
+	languages           = []LangItem{
+		{"en", "英语"},
+		{"zh_cn", "中文"},
+	}
 )
 
 func GetSubripTranslate() *SubripTranslate {
@@ -48,8 +58,34 @@ func (customPage *SubripTranslate) SetVisible(isVisible bool) {
 }
 
 func (customPage *SubripTranslate) GetWidget() Widget {
+	var tmpCombo *walk.ComboBox
 	return StdRootWidget(&customPage.rootWidget,
-		pack.TTLabel(pack.NewTTLabelArgs(nil).SetText("神秘代码：字幕翻译")),
+		pack.TTComposite(pack.NewTTCompositeArgs(nil).
+			SetLayoutHBox(true).SetWidgets(
+			pack.NewWidgetGroup().Append(
+				pack.TTLabel(pack.NewTTLabelArgs(nil).SetText("翻译引擎")),
+				pack.TTComboBox(pack.NewTTComboBoxArgs(nil).
+					SetModel([]string{"阿里云", "百度云", "腾讯云", "ChatGPT"}).SetCurrentIdx(0)),
+				pack.TTLabel(pack.NewTTLabelArgs(nil).SetText("来源语种")),
+				pack.TTComboBox(pack.NewTTComboBoxArgs(nil).
+					SetModel(ling_va.GetInstance().GetLangSupported()).SetBindingMember("Key").SetDisplayMember("Name").SetCurrentIdx(0)),
+				pack.TTLabel(pack.NewTTLabelArgs(nil).SetText("目标语种")),
+				pack.TTComboBox(pack.NewTTComboBoxArgs(&tmpCombo).
+					SetModel(ling_va.GetInstance().GetLangSupported()).SetBindingMember("Key").SetDisplayMember("Name").SetCurrentIdx(1)),
+			).AppendZeroHSpacer().GetWidgets(),
+		)),
+		pack.TTComposite(pack.NewTTCompositeArgs(nil).
+			SetLayoutHBox(true).SetWidgets(
+			pack.NewWidgetGroup().Append(
+				pack.TTLabel(pack.NewTTLabelArgs(nil).SetText("翻译模式")),
+				pack.TTComboBox(pack.NewTTComboBoxArgs(nil).
+					SetModel([]string{"全量翻译", "增量翻译"}).SetCurrentIdx(0)),
+				pack.TTLabel(pack.NewTTLabelArgs(nil).SetText("导出主轨道")),
+				pack.TTComboBox(pack.NewTTComboBoxArgs(nil).
+					SetModel([]string{"来源语种", "目标语种"}).SetCurrentIdx(0)),
+			).AppendZeroHSpacer().GetWidgets(),
+		)),
+		VSpacer{},
 	)
 }
 
